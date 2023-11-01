86325286f7e8e4d565d10bbe09b1d6ab00e87c4b
notsokodya <66978308+notsokodya@users.noreply.github.com>
2023-11-01 11:50:27 +0300

    Added eslint and "transliterate" command
    Added eslint
    - no way
    
    Added "transliterate" command
    - has 2 option
    - 1. lang (en / ru)
    - 2. text (required)

---
+++ commands/utility/transliterate.js
@@ -0,0 +1,52 @@
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+
+const en = "qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?</>@#$^&`~";
+const ru = "йцукенгшщзхъфывапролджэячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,\"№;:?ёЁ";
+let toEN = {}, toRU = {};
+
+for ( let i = 0; i < en.length; ++i ) {
+    const ru_char = ru[i], 
+          en_char = en[i];
+    toEN[ru_char] = en_char;
+    toRU[en_char] = ru_char;
+}
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("transliterate")
+        .setDescription("Transliterates text")
+        .addStringOption(option => 
+            option
+            .setName("lang")
+            .setDescription("Language to transliterate text")
+            .addChoices(
+                {name: "to Russian", value: "ru"},
+                {name: "to English", value: "en"}
+            )
+        )
+        .addStringOption(option => {
+            option
+            .setName("text")
+            .setDescription("Text to transliterate")
+            .setRequired(true)
+        }),
+    async execute(interaction) {
+        const lang = interaction.options.getString("lang") || "ru";
+        const text = interaction.options.getString("text");
+        let transliterated = "";
+
+        const langTranslit = lang === "ru" ? toRU : toEN;
+        for ( let i = 0; i < text.length; ++i ) {
+            transliterated += langTranslit[text[i]] || text[i]
+        }
+
+        const embed = new EmbedBuilder()
+            .setTitle("Transliterated text")
+            .setDescription(`${transliterated}`)
+            .setColor("Aqua");
+
+        await interaction.reply({
+            embeds: [embed]
+        });
+    }
+};
